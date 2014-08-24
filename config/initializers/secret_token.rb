68fa3a028c1bfa26410c140a95f6556c5a0c7a5e
Clint Helling <clint.helling@gmail.com>
2014-08-24 16:42:03 -0700

    Hide site secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Whiteboard::Application.config.secret_token = '599ff48fe7e09aa8d7c961269d438776d369799ac7316a4eab53d7430b072c77086e633884b5554357745ebbd182abc6f443e4f2fd6c4a94cb95eb3304c6e33c'
+Whiteboard::Application.config.secret_token = ENV['WHITEBOARD_SECRET_KEY']
