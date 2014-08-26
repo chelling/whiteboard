110496823b783221ed31fe5062e22e05bbd8438c
Clint Helling <clint.helling@gmail.com>
2014-08-26 13:39:22 -0700

    Fixed win pool pick email bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,7 @@ class UserMailer < ActionMailer::Base
   def win_pool_pick
     User.all.map do |user|
       @win_pool_pick = WinPoolPick.find_by_user_id_and_year_and_win_pool_league_id(user.id, Date.today.year, 1)
-      @is_my_pick = @win_pool_pick.is_current_user_turn? unless @win_pool_pick.nil?
-      if @is_my_pick
+      if !@win_pool_pick.nil? && @win_pool_pick.is_current_user_turn?
         mail(:to => user.email, :subject => 'It\'s your turn to pick!')
       end
     end
