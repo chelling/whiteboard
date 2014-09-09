7a784fd10cdef4bd8772474b4449c4906c026999
Clint Helling <clint.helling@gmail.com>
2014-09-09 09:14:05 -0700

    Fixed week bug in fooicide

--- app/controllers/pickem_picks_controller.rb
+++ app/controllers/pickem_picks_controller.rb
@@ -181,20 +181,19 @@ class PickemPicksController < ApplicationController
           authorize! :create, pick
         end
 
-        if !pick.update_attributes(:user_id => current_user.id, :game_id => game.last, :team_id => value, :week => @week, :year => @year)
-          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Error while updating your picks.'
+        if !pick.update_attributes(:user_id => current_user.id, :game_id => game.last, :team_id => value, :week => week.last, :year => @year)
+          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Error while updating your picks.'
         end
       end
 
       # set wagers as well for single picks
       if game.first == "wager" && !value.empty?
-        puts "\n#{game}"
-        puts "\n#{value}"
+
         pick = PickemPick.find_by_user_id_and_game_id(current_user.id, game.last)
         if pick.nil?
-          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Cannot bet without making a pick'
+          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Cannot bet without making a pick'
         elsif pick.game.in_progress_or_complete?
-          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Cannot bet when game is in progress'
+          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Cannot bet when game is in progress'
         end
         authorize! :update, pick
 
