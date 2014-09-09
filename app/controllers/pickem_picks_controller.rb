7f623c22363ab8854284b6298a85537a9e2ce925
Clint Helling <clint.helling@gmail.com>
2014-09-09 09:35:50 -0700

    Final week bug fix

--- app/controllers/pickem_picks_controller.rb
+++ app/controllers/pickem_picks_controller.rb
@@ -181,8 +181,8 @@ class PickemPicksController < ApplicationController
           authorize! :create, pick
         end
 
-        if !pick.update_attributes(:user_id => current_user.id, :game_id => game.last, :team_id => value, :week => week.last, :year => @year)
-          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Error while updating your picks.'
+        if !pick.update_attributes(:user_id => current_user.id, :game_id => game.last, :team_id => value, :week => @week, :year => @year)
+          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Error while updating your picks.'
         end
       end
 
@@ -191,9 +191,9 @@ class PickemPicksController < ApplicationController
 
         pick = PickemPick.find_by_user_id_and_game_id(current_user.id, game.last)
         if pick.nil?
-          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Cannot bet without making a pick'
+          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Cannot bet without making a pick'
         elsif pick.game.in_progress_or_complete?
-          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Cannot bet when game is in progress'
+          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Cannot bet when game is in progress'
         end
         authorize! :update, pick
 
@@ -206,7 +206,7 @@ class PickemPicksController < ApplicationController
         end
 
         if !wager.update_attributes(:account_id => current_user.accounts.find_by_year(@year).try(:id), :pickem_pick_id => pick.id, :amount => value)
-          return redirect_to "/pickem?year=#{@year}&week=#{week.last}", alert: 'Error while updating your wagers.'
+          return redirect_to "/pickem?year=#{@year}&week=#{@week}", alert: 'Error while updating your wagers.'
         end
       end
     end
