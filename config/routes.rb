977403fde0796adf479aca5afe07861dbb81a4d2
Clint Helling <clint.helling@gmail.com>
2015-07-18 11:31:42 -0700

    Fix database for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Whiteboard::Application.routes.draw do
-  root :to => "home#index"
+  root to: "home#index"
   
-  match "/scores" => "games#scores"
-  match "/pickem" => "pickem_picks#scores"
-  match "/pickem_pick/update_picks" => "pickem_picks#update_picks"
-  match "/stats" => "pickem_picks#stats"
-  match "/fooicide" => "fooicide_picks#scores"
-  match "/fooicide/rules" => "fooicide_picks#rules"
-  match "/fooicide/update_picks" => "fooicide_picks#update_picks"
-  match "/winpool/:id" => "win_pool_picks#win_pool"
-  match "/winpool/:id/pick_team" => "win_pool_picks#pick_team"
-  match "/user_wagers/:year/:week/:user_id" => "home#user_wagers"
-  match "/user_wagers/:year/:user_id" => "home#user_wagers"
-  match "/update_games" => "pickem_picks#update_games"
+  get "/scores", to: "games#scores"
+  get "/pickem", to: "pickem_picks#scores"
+  get "/pickem_pick/update_picks", to:"pickem_picks#update_picks"
+  get "/stats", to: "pickem_picks#stats"
+  get "/fooicide", to: "fooicide_picks#scores"
+  get "/fooicide/rules", to: "fooicide_picks#rules"
+  get "/fooicide/update_picks", to: "fooicide_picks#update_picks"
+  get "/winpool/:id", to: "win_pool_picks#win_pool"
+  get "/winpool/:id/pick_team", to: "win_pool_picks#pick_team"
+  get "/user_wagers/:year/:week/:user_id", to: "home#user_wagers"
+  get "/user_wagers/:year/:user_id", to: "home#user_wagers"
+  get "/update_games", to: "pickem_picks#update_games"
 
   # match "/thirtyeight" => "thirty_eights#scores"
   # match "/thirtyeight" => "thirty_eights#rules"
@@ -28,61 +28,4 @@ Whiteboard::Application.routes.draw do
   resources :stadiums
 
   devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,34 +9,37 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140706191709) do
+ActiveRecord::Schema.define(version: 20140706191709) do
 
-  create_table "accounts", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "year"
     t.decimal  "amount"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "accounts", ["user_id"], :name => "index_accounts_on_user_id"
+  add_index "accounts", ["user_id"], name: "index_accounts_on_user_id", using: :btree
 
-  create_table "fooicide_picks", :force => true do |t|
+  create_table "fooicide_picks", force: :cascade do |t|
     t.integer  "year"
     t.integer  "week"
     t.integer  "game_id"
     t.integer  "user_id"
     t.integer  "team_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "win"
   end
 
-  add_index "fooicide_picks", ["year", "week", "user_id"], :name => "index_fooicide_picks_on_year_and_week_and_user_id"
+  add_index "fooicide_picks", ["year", "week", "user_id"], name: "index_fooicide_picks_on_year_and_week_and_user_id", using: :btree
 
-  create_table "games", :force => true do |t|
+  create_table "games", force: :cascade do |t|
     t.integer  "year"
     t.integer  "week"
     t.string   "location"
@@ -45,130 +48,130 @@ ActiveRecord::Schema.define(:version => 20140706191709) do
     t.integer  "home_score"
     t.integer  "away_score"
     t.datetime "date"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.float    "line"
   end
 
-  add_index "games", ["year", "week"], :name => "index_games_on_year_and_week"
+  add_index "games", ["year", "week"], name: "index_games_on_year_and_week", using: :btree
 
-  create_table "pickem_picks", :force => true do |t|
+  create_table "pickem_picks", force: :cascade do |t|
     t.integer  "year"
     t.integer  "week"
     t.integer  "game_id"
     t.integer  "user_id"
     t.integer  "team_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "win"
     t.boolean  "tie"
     t.boolean  "recommended"
     t.integer  "recommended_points"
   end
 
-  add_index "pickem_picks", ["year", "week", "user_id"], :name => "index_pickem_picks_on_year_and_week_and_user_id"
+  add_index "pickem_picks", ["year", "week", "user_id"], name: "index_pickem_picks_on_year_and_week_and_user_id", using: :btree
 
-  create_table "records", :force => true do |t|
+  create_table "records", force: :cascade do |t|
     t.integer  "team_id"
     t.integer  "year"
     t.integer  "wins"
     t.integer  "losses"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "records", ["team_id", "year"], :name => "index_records_on_team_id_and_year"
+  add_index "records", ["team_id", "year"], name: "index_records_on_team_id_and_year", using: :btree
 
-  create_table "shares", :force => true do |t|
+  create_table "shares", force: :cascade do |t|
     t.integer  "year"
     t.integer  "user_id"
     t.integer  "game_id"
     t.integer  "team_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "shares", ["user_id", "team_id", "game_id"], :name => "index_shares_on_user_id_and_team_id_and_game_id"
+  add_index "shares", ["user_id", "team_id", "game_id"], name: "index_shares_on_user_id_and_team_id_and_game_id", using: :btree
 
-  create_table "stadiums", :force => true do |t|
+  create_table "stadiums", force: :cascade do |t|
     t.integer  "team_id"
     t.string   "location"
     t.string   "time_zone"
     t.string   "stadium_type"
     t.string   "grass_type"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
   end
 
-  create_table "teams", :force => true do |t|
+  create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
     t.string   "image"
     t.string   "conference"
     t.string   "division"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "teams", ["conference", "division"], :name => "index_teams_on_conference_and_division"
+  add_index "teams", ["conference", "division"], name: "index_teams_on_conference_and_division", using: :btree
 
-  create_table "thirty_eights", :force => true do |t|
+  create_table "thirty_eights", force: :cascade do |t|
     t.integer  "year"
     t.integer  "team_id"
     t.integer  "user_id"
     t.integer  "shares"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "thirty_eights", ["year", "user_id"], :name => "index_thirty_eights_on_year_and_user_id"
+  add_index "thirty_eights", ["year", "user_id"], name: "index_thirty_eights_on_year_and_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "password_salt"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
     t.boolean  "admin"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "wagers", :force => true do |t|
+  create_table "wagers", force: :cascade do |t|
     t.integer  "account_id"
     t.integer  "pickem_pick_id"
     t.decimal  "amount"
     t.decimal  "potential_payout"
     t.decimal  "payout"
     t.integer  "win"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.decimal  "previous_amount"
   end
 
-  add_index "wagers", ["account_id", "pickem_pick_id"], :name => "index_wagers_on_account_id_and_pickem_pick_id"
+  add_index "wagers", ["account_id", "pickem_pick_id"], name: "index_wagers_on_account_id_and_pickem_pick_id", using: :btree
 
-  create_table "win_pool_leagues", :force => true do |t|
+  create_table "win_pool_leagues", force: :cascade do |t|
     t.string   "name"
     t.integer  "year"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "win_pool_leagues", ["year"], :name => "index_win_pool_leagues_on_year"
+  add_index "win_pool_leagues", ["year"], name: "index_win_pool_leagues_on_year", using: :btree
 
-  create_table "win_pool_picks", :force => true do |t|
+  create_table "win_pool_picks", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "win_pool_league_id"
     t.integer  "starting_position"
@@ -176,10 +179,10 @@ ActiveRecord::Schema.define(:version => 20140706191709) do
     t.integer  "team_one_id"
     t.integer  "team_two_id"
     t.integer  "team_three_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "win_pool_picks", ["win_pool_league_id", "user_id", "year"], :name => "index_win_pool_picks_on_win_pool_league_id_and_user_id_and_year"
+  add_index "win_pool_picks", ["win_pool_league_id", "user_id", "year"], name: "index_win_pool_picks_on_win_pool_league_id_and_user_id_and_year", using: :btree
 
 end
