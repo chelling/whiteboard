95a9049f6820030fb73444b922619ddf290c84da
Clint Helling <clint.helling@gmail.com>
2015-09-07 10:59:54 -0700

    Fix date bug

--- app/models/game.rb
+++ app/models/game.rb
@@ -592,13 +592,13 @@ class Game < ActiveRecord::Base
       if game.nil? && !Team.find_by(name: away_team).nil?  && !Team.find_by(name: home_team).nil?
         Game.create(:away_team_id => Team.find_by(name: away_team).try(:id), \
                     :home_team_id => Team.find_by(name: home_team).try(:id), \
-                    :date => DateTime.strptime(date + " 2014 " + time, '%a, %b %d %Y %H:%M %p'), \
+                    :date => DateTime.strptime(date + " 2015 " + time, '%a, %b %d %Y %H:%M %p'), \
                     :location => Team.find_by(name: home_team).try(:location), :year => year, :week => week,
                     :away_score => away_score, :home_score => home_score)
       elsif !game.nil? && time.include?('FINAL')
         game.update_attributes(:away_team_id => Team.find_by(name: away_team).try(:id), \
                     :home_team_id => Team.find_by(name: home_team).try(:id), \
-                    # :date => DateTime.strptime(date + " 2014 " + time, '%a, %b %d %Y %H:%M %p'), \
+                    # :date => DateTime.strptime(date + " 2015 " + time, '%a, %b %d %Y %H:%M %p'), \
                     :location => Team.find_by(name: home_team).try(:location), :year => year, :week => week,
                     :away_score => away_score, :home_score => home_score)
         game.save
@@ -629,13 +629,13 @@ class Game < ActiveRecord::Base
       if game.nil? && !Team.find_by(name: away_team).nil?  && !Team.find_by(name: home_team).nil?
         Game.create(:away_team_id => Team.find_by(name: away_team).try(:id), \
                     :home_team_id => Team.find_by(name: home_team).try(:id), \
-                    :date => DateTime.strptime(date + " 2014 " + time, '%a, %b %d %Y %H:%M %p'), \
+                    :date => DateTime.strptime(date + " 2015 " + time, '%a, %b %d %Y %H:%M %p'), \
                     :location => Team.find_by(name: home_team).try(:location), :year => year, :week => week,
                     :away_score => away_score, :home_score => home_score)
       elsif !game.nil?
         game.update_attributes(:away_team_id => Team.find_by(name: away_team).try(:id), \
                     :home_team_id => Team.find_by(name: home_team).try(:id), \
-                    :date => DateTime.strptime(date + " 2014 " + time, '%a, %b %d %Y %H:%M %p'), \
+                    :date => DateTime.strptime(date + " 2015 " + time, '%a, %b %d %Y %H:%M %p'), \
                     :location => Team.find_by(name: home_team).try(:location), :year => year, :week => week,
                                :away_score => away_score, :home_score => home_score)
         game.save
