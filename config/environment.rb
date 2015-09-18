da64e4c99879440e06e330a2607d2f1ea10d67ba
Clint Helling <clint.helling@gmail.com>
2015-09-17 22:13:40 -0700

    Week to_s

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ def find_week
   week = (((@time - week1) / (3600 * 24)) / 7).ceil
   return '1' if week <= 0
   return '17' if week >= 17
-  return week
+  return week.to_s
 end
 
 # Initialize the rails application
