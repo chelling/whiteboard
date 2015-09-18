e4cecd75b2afdfc71452e8e823b7156cca699606
Clint Helling <clint.helling@gmail.com>
2015-09-17 22:09:19 -0700

    Fix week

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 def find_week
-  week1 = Time.new(2015, 9, 16)
+  week1 = Time.new(2015, 9, 9)
   @time = Time.now.in_time_zone('Arizona')
-  week = (((@time - week1) / (3600 * 24)) / 7).to_i
+  week = (((@time - week1) / (3600 * 24)) / 7).ceil
   return '1' if week <= 0
   return '17' if week >= 17
   return week
