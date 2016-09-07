8ebfc4804b68a8d875c59211af043848242fe466
Clint Helling <clint.helling@gmail.com>
2016-09-06 22:03:13 -0700

    Updating fooicide to 2016

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 def find_week
-  week1 = Time.new(2015, 9, 9)
+  week1 = Time.new(2016, 9, 13)
   @time = Time.now.in_time_zone('Arizona')
   week = (((@time - week1) / (3600 * 24)) / 7).ceil
   return '1' if week <= 0
